bb8b4476ecda0278939831be556f784d173e84a6
Andrew Pollock <apollock@debian.org>
2015-03-07 08:49:08 +1000

    Initial commit of functioning extension

---
+++ lwn.js
@@ -0,0 +1,66 @@
+function getAndSetTitle(request, url, rewritten) {
+  return function() {
+    var link = url;
+
+    if (request.readyState == 4) {
+      console.log('Attempting to parse ' + link);
+      var parser = new DOMParser();
+      var dom = parser.parseFromString(request.responseText, 'text/html');
+      // TODO(apollock): check if anything malicious can be injected in here
+      var title = dom.title;
+      // This makes me sad, but seems to be the only way to safely operate on
+      // the right link. The index into document.links doesn't seem to stay
+      // stable between the intial iteration over it and when the callback
+      // fires.
+      if (rewritten) {
+        link = link.replace(/^https:/i, 'http:');
+      }
+      console.log('Searching for ' + link);
+      for (i = 0; i < document.links.length; i++) {
+        if (document.links.item(i).href == link) {
+          console.log('Setting title to ' + title);
+          document.links.item(i).title = title;
+          if (document.links.item(i).title != title) {
+            console.log('Setting titled failed!');
+          }
+          // And we can't even break out of the loop early, because the same
+          // link could appear multiple times. Sadness to the extreme.
+          // break;
+        }
+      }
+    }
+  }
+}
+
+function setTitle(url, rewritten) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+  var callback = getAndSetTitle(xhr, url, rewritten);
+  xhr.onreadystatechange = callback;
+  xhr.send();
+}
+
+for (i = 0; i < document.links.length; i++) {
+  var lwnArticleUrl = /^(http[s]?:)\/\/lwn\.net\/Articles\//i;
+
+  if (lwnArticleUrl.test(document.links.item(i).href) &&
+      document.links.item(i).title == '') {
+    var link = document.links.item(i).href;
+    var rewritten = false;
+
+    // If we're HTTPS and one of the links is an absolute HTTP link, rewrite it
+    // to HTTPS to avoid a mixed content failure
+    if (window.location.protocol == 'https:') {
+      var matches = lwnArticleUrl.exec(document.links.item(i).href);
+      if (matches[1] != 'https:') {
+        console.log('Remapping protocol to HTTPS for ' + link);
+        link = link.replace(/^http:/i, 'https:');
+        rewritten = true;
+      }
+    }
+    console.log('Calling setTitle() for link ' + i + ', which is ' + link +
+        ' with rewritten = ' + rewritten);
+    setTitle(link, rewritten);
+  }
+}
